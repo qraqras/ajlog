33832dede34ca69467cc69d6f36973be247880f3
qraqras <q100rrraaa@gmail.com>
2025-07-22 10:14:24 +0900

    Backend: Add test

---
+++ backend/ajlog/src/test_main.py
@@ -0,0 +1,32 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel.pool import StaticPool
+
+from .main import app, get_session
+
+# https://sqlmodel.tiangolo.com/tutorial/fastapi/tests/
+
+@pytest.fixture(name="session")
+def session_fixture():
+    engine = create_engine(
+        "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
+    )
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
+
+
+@pytest.fixture(name="client")
+def client_fixture(session: Session):
+    def get_session_override():
+        return session
+
+    app.dependency_overrides[get_session] = get_session_override
+
+    client = TestClient(app)
+    yield client
+    app.dependency_overrides.clear()
+
+def test_sample(client: TestClient):
+    assert False
