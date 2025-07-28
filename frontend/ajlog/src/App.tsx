bd40b0a7d80a2002ca91af6f79e40fee0981bd33
qraqras <q100rrraaa@gmail.com>
2025-07-28 23:39:02 +0900

    Frontend: Organize Routes

--- frontend/ajlog/src/App.tsx
+++ frontend/ajlog/src/App.tsx
@@ -4,15 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import { Routes, Route, Link } from 'react-router';
-import { Scrum } from './scrum.tsx';
 import { Dnd } from "./dnd.tsx";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/scrum" element={<Scrum />} />
-        <Route path="/dnd" element={<Dnd />} />
+        <Route path="" element={<Dnd />} />
       </Routes>
     </>
   )
