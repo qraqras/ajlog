074437a478a21a15d172d7d4b16d64384ad1a671
qraqras <q100rrraaa@gmail.com>
2025-08-01 14:35:31 +0900

    Frontend: Delete unnecessary files

--- frontend/ajlog/src/main.tsx
+++ frontend/ajlog/src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
+
 import './index.css'
-import App from './App.tsx'
+import { Dnd } from "./dnd.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <h1 className="text-3xl font-bold underline">AjLog</h1>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="" element={<Dnd />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
